e96535de7adfab4d6c63a5d743a28a216c72a171
Allan <allanfdsz@gmail.com>
2019-08-29 21:39:44 -0300

    init css

--- view/cadastrar.php
+++ view/cadastrar.php
@@ -16,6 +16,25 @@
 <?php
   include("../view/menu.php");
 ?>
+<forms action="">
+  <div class="forms-group">
+   <label for="nome">Nome</label><span class"text-red">*</span>
+   <input type="text" name="nome" id="nome" required class="form-control">
+  </div>
+
+  <div class="forms-group">
+   <label for="Telefone">Telefone</label><span class"text-red">*</span>
+   <input type="text" name="telefone" id="telefone" required class="form-control">
+  </div>
+
+   <div class="forms-group">
+   <label for="Celular">Celular</label><span class"text-red">*</span>
+   <input type="text" name="celular" id="celular" required class="form-control">
+  </div>
+  
+</forms>
+
+
 <!--  Aqui podemos colocar o codigo HTML -->
 <?php
   include("../js/bootstrap.php");
